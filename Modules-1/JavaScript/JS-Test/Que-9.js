290b92594eeb3efc49a210b44d1f7f6d9175fea2
Ravi Shankar Singh <civilstudy2024@gmail.com>
2024-10-09 10:36:42 +0530

    JavaScript Test Complited

--- Modules-1/JavaScript/JS-Test/Que-9.js
+++ Modules-1/JavaScript/JS-Test/Que-9.js
@@ -6,5 +6,15 @@ the output should be 5 (0, 1, 1, 2, 3, 5).
 
 */
 
+let x=0, y=1, z;
+console.log("fibonacci numbers");
+for(let i=0; i<=5; i++){
+    console.log(x);
+    z=x+y;
+    x=y;
+    y=z;
+}
+
+
 
 
